4f12bc48cb7aed633abd65b71b22e281c07b0774
chelovekm <161503290+chelovekm@users.noreply.github.com>
2025-09-04 14:11:36 +0300

    Update White.cs

--- Lab1/White.cs
+++ Lab1/White.cs
@@ -7,7 +7,7 @@
             bool answer = false;
 
             // code here
-
+            // 2
             // end
 
             return answer;
@@ -84,4 +84,5 @@
             return answer;
         }
     }
+
 }
