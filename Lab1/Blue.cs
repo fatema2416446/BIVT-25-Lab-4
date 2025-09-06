506c784c09c21508ffa421feb2d42a9d2271c4f1
kaliexp <m2514958@edu.misis.ru>
2025-09-06 22:07:47 +0300

    completed

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -5,81 +5,86 @@
         public bool Task1(int a, int b)
         {
             bool answer = false;
-
-            // code here
-
-            // end
-
+            if (a != 0 && b != 0 && a * b > 0)
+            {
+                answer = true;
+            }
             return answer;
         }
         public bool Task2(double d)
         {
             bool answer = false;
-
-            // code here
-
-            // end
-
+            double fractionalPart = Math.Abs(d - Math.Truncate(d));
+            if (fractionalPart >= 0.0001)
+            {
+                answer = true;
+            }
             return answer;
         }
         public bool Task3(int a, int b)
         {
             bool answer = false;
-
-            // code here
-
-            // end
-
+            if (b != 0 && a % b == 0)
+            {
+                answer = true;
+            }
             return answer;
         }
         public double Task4(double d, double f, double g)
         {
             double answer = 0;
-
-            // code here
-
-            // end
-
+            if (Math.Abs(d) >= Math.Abs(f) && Math.Abs(d) >= Math.Abs(g))
+                answer = d;
+            else if (Math.Abs(f) >= Math.Abs(g))
+                answer = f;
+            else
+                answer = g;
             return answer;
         }
         public double Task5(double x)
         {
             double answer = 0;
+            if (x <= -1)
+                answer = 0;
+            else if (x <= 0)
+                answer = x + 1;
+            else
+                answer = 1;
 
-            // code here
-
-            // end
 
             return answer;
         }
         public bool Task6(double circleS, double squareS)
         {
             bool answer = false;
-
-            // code here
-
-            // end
-
+            if (2 * Math.Sqrt(circleS / Math.PI) <= Math.Sqrt(squareS))
+                answer = true;
             return answer;
         }
 
         public double Task7(double d, double f)
         {
             int answer = 0;
-
-            // code here
-
-            // end
-
+            if (Math.Abs(d) < Math.Abs(f))
+            {
+                if (d > 0)
+                    answer = -1;
+            }
+            else
+            {
+                if (f > 0)
+                    answer = 1;
+            }
             return answer;
         }
         public bool Task8(int a, int b, int c)
         {
             bool answer = false;
-
-            // code here
-            
-            // end
+            int A = a / 2;
+            int B = b / 2;
+            int C = c / 2;
+            if (A >= 1 && A == B && (C == A || C + 1 == A))
+                return true;
 
             return answer;
         }
