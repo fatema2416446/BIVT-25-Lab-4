0177e91a24471477f7b9eefd5326fb0c321232e4
Maria-Myagkova <maromiagkova@gmail.com>
2025-09-10 21:18:16 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,14 +4,19 @@
     {
         public static void Main()
         {
-            White white = new White();
-            Console.WriteLine(white.Task1(1.2));
-            Console.WriteLine(white.Task2(2));
-            Console.WriteLine(white.Task3(2, 1));
-            Console.WriteLine(white.Task4(-2.5, 1.89));
-            Console.WriteLine(white.Task5(0.5));
-            Console.WriteLine(white.Task6(2.5, 1.89, 3));
-            Console.WriteLine(white.Task7(2));
+            Blue blue = new Blue();
+            Console.WriteLine(blue.Task1(1, 1));
+            Console.WriteLine(blue.Task2(-10.230001));
+            Console.WriteLine(blue.Task3(-125, -5));
+            Console.WriteLine(blue.Task4(-2.5, -2.5, -2.5));
+            Console.WriteLine(blue.Task5(-0.5));
+            Console.WriteLine(blue.Task6(78.5, 100));
+            Console.WriteLine(blue.Task7(2.4, 3.5));
+            Console.WriteLine(blue.Task8(5, 4, 3));
+
+
+
+
         }
     }
 }
