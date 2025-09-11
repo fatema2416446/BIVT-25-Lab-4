4e58baf87f6e182fb742a321df38e8580cb9881f
emreesss <artemglavatskikh198@gmail.com>
2025-09-11 15:49:14 +0300

    Update Blue.cs

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -7,7 +7,13 @@
             bool answer = false;
 
             // code here
-
+            if (a != 0 && b != 0)
+            {
+                if ((a > 0 && b > 0) || (a < 0 && b < 0))
+                {
+                    answer = true;
+                }
+            }
             // end
 
             return answer;
@@ -17,7 +23,10 @@
             bool answer = false;
 
             // code here
-
+            if (Math.Floor(d) < d)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -27,7 +36,13 @@
             bool answer = false;
 
             // code here
-
+            if (b != 0)
+            {
+                if (a % b == 0)
+                {
+                    answer = true;
+                }
+            }
             // end
 
             return answer;
@@ -37,7 +52,22 @@
             double answer = 0;
 
             // code here
-
+            if (Math.Abs(d) > Math.Abs(f) && Math.Abs(d) > Math.Abs(g))
+            {
+                answer = d;
+            }
+            else if (Math.Abs(f) > Math.Abs(d) && Math.Abs(f) > Math.Abs(g))
+            {
+                answer = f;
+            }
+            else if (Math.Abs(g) > Math.Abs(d) && Math.Abs(g) > Math.Abs(f))
+            {
+                answer = g;
+            }
+            else
+            {
+                answer = g;
+            }
             // end
 
             return answer;
@@ -47,7 +77,18 @@
             double answer = 0;
 
             // code here
-
+            if (x <= -1)
+            {
+                answer = 0;
+            }
+            else if (x > -1 && x <= 0)
+            {
+                answer = x+1;
+            }
+            else
+            {
+                answer = 1;
+            }
             // end
 
             return answer;
@@ -57,7 +98,10 @@
             bool answer = false;
 
             // code here
-
+            if (Math.Sqrt(squareS) / 2 >= Math.Sqrt(circleS / Math.PI))
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -68,7 +112,20 @@
             int answer = 0;
 
             // code here
-
+            if (Math.Abs(d) < Math.Abs(f))
+            {
+                if (d > 0)
+                {
+                    answer = -1;
+                }
+            }
+            else
+            {
+                if (f > 0)
+                {
+                    answer = 1;
+                }
+            }
             // end
 
             return answer;
@@ -78,10 +135,26 @@
             bool answer = false;
 
             // code here
-            
+            int a1 = a / 2;
+            int b1 = b / 2;
+            int c1 = c / 2;
+            int sum = a1 + b1 + c1;
+            int sum1 = sum / 3;
+            if (sum % 3 == 0 && sum >=3)
+            {
+                answer = true;
+            }
+            else if (sum % 3 != 0)
+            {
+                if (sum - ((sum1 +1)*3 - 1) ==0)
+                {
+                    answer = true;
+                }
+            }
             // end
 
             return answer;
         }
     }
+
 }
