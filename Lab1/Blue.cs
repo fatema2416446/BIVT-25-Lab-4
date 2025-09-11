3f5ec01db113777018533effae725c1fe9a867bc
Maria-Myagkova <maromiagkova@gmail.com>
2025-09-11 14:28:16 +0300

    Add files via upload

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab1
+﻿using System.Threading.Channels;
+
+namespace Lab1
 {
     public class Blue
     {
@@ -6,18 +8,28 @@
         {
             bool answer = false;
 
+
             // code here
+            if ((a > 0) && (b > 0))
+                answer = true;
+            else if ((a < 0) && (b < 0))
+                answer = true;
+            else if ((a == b) && (b == 0))
+                answer = true;
 
-            // end
 
-            return answer;
+                // end
+
+                return answer;
         }
         public bool Task2(double d)
         {
             bool answer = false;
 
             // code here
-
+            d = Math.Abs(d);
+            if (((d % 1)*10000) >= 1)
+                    answer = true;
             // end
 
             return answer;
@@ -27,7 +39,8 @@
             bool answer = false;
 
             // code here
-
+            if ((b != 0) && (a % b == 0))
+                answer = true;
             // end
 
             return answer;
@@ -37,7 +50,16 @@
             double answer = 0;
 
             // code here
-
+            double d1 = Math.Abs(d);
+            double f1 = Math.Abs(f);
+            double g1 = Math.Abs(g);
+            if ((d1 >= f1) && (d1 >= g1))
+                answer = d;
+            else if ((f1 >= d1) && (f1 >= g1))
+                answer = f;
+            else if ((g1 >= f1) && (g1 >= d1))
+                answer = g;
+            
             // end
 
             return answer;
@@ -47,6 +69,14 @@
             double answer = 0;
 
             // code here
+            if (x <= -1)
+                answer = 0;
+            else if ((x <= 0) && (x > -1))
+                answer = x + 1;
+            else
+            {
+                answer = 1;
+            }
 
             // end
 
@@ -57,7 +87,10 @@
             bool answer = false;
 
             // code here
-
+            double x = Math.Pow(squareS, 0.5);
+            double r = Math.Pow((circleS / Math.PI), 0.5);
+            if ((squareS >= circleS) && (x >= (2 * r)))
+                answer = true;
             // end
 
             return answer;
@@ -68,20 +101,46 @@
             int answer = 0;
 
             // code here
+            if (Math.Abs(d) < Math.Abs(f))
+            {
+                if (d > 0)
+                    answer = -1;
+            }
+            else
+            {
+                if (f > 0)
+                    answer = 1;
+            }
 
-            // end
+                // end
 
-            return answer;
+                return answer;
         }
         public bool Task8(int a, int b, int c)
         {
             bool answer = false;
 
             // code here
-            
-            // end
+            double a1 = a / 2;
+            double b1 = b / 2;
+            double c1 = c / 2;
+            int mid_a = (int)Math.Floor(a1);
+            int mid_b = (int)Math.Floor(b1);
+            int mid_c = (int)Math.Floor(c1);
+            if ((mid_a + mid_b + mid_c) >= 3)
+            {
+                if ((mid_a + mid_b + mid_c) % 3 == 0)
+                    answer = true;
+                else if ((mid_a + mid_b + mid_c) % 3 == 2) // добавил монету младшему
+                    answer = true;
+            }
+            else if ((mid_a + mid_b + mid_c) == 2) // добавил монету младшему
+                answer = true;
 
-            return answer;
+
+                // end
+
+                return answer;
         }
     }
 }
