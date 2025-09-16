abcb4703e1e73255bbe0c0e1fe0d85df2daa29b5
ХерелКристина <kherelkristina6b@mail.ru>
2025-09-16 11:48:35 +0300

    Add files via upload

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -7,17 +7,23 @@
             bool answer = false;
 
             // code here
-
+            if(a*b>0 || a==b)
+            {
+                answer = true;
+            }
             // end
 
-            return answer;
+                return answer;
         }
         public bool Task2(double d)
         {
             bool answer = false;
 
             // code here
-
+            if (0 <( Math.Round(Math.Abs(d % 1),4)) && (Math.Round(Math.Abs(d%1),4))<1)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -27,37 +33,65 @@
             bool answer = false;
 
             // code here
+            if(b != 0 && a %b==0)
+            {
+                answer=true;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public double Task4(double d, double f, double g)
         {
             double answer = 0;
 
             // code here
+            if(Math.Abs(d)>Math.Abs(f) && Math.Abs(d)>Math.Abs(g))
+            {
+                answer = d;
+            }
+            else if(Math.Abs(f)>Math.Abs(d) && Math.Abs(f)>Math.Abs(g))
+            {
+                answer = f;
+            }
+            else
+            {
+                answer = g;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public double Task5(double x)
         {
             double answer = 0;
 
             // code here
+            if(x<=-1)
+            {
+                answer = 0;
+            }
+            else if(x<=0)
+            {
+                answer = x + 1;
+            }
+            else
+            {
+                answer = 1;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task6(double circleS, double squareS)
         {
             bool answer = false;
 
             // code here
-
+            if(Math.Sqrt((circleS/Math.PI))<=Math.Sqrt((squareS/4)))
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -68,17 +102,30 @@
             int answer = 0;
 
             // code here
+            if(Math.Abs(d)<Math.Abs(f))
+            {
+                if (d > 0)
+                    answer = -1;
+            }
+            else
+            {
+                if (f > 0)
+                    answer = 1;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task8(int a, int b, int c)
         {
             bool answer = false;
 
             // code here
-            
+            int s = (a / 2) + (b / 2) + (c / 2);
+            if(s>0 &&(s%3==0 || s%3==2))
+            {
+                answer = true;
+            }
             // end
 
             return answer;
