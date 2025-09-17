67377be912cb12e3d9add1f7646289ea0831f7cc
Marina Dmitrieva <70883784+M8r1na@users.noreply.github.com>
2025-09-18 00:13:11 +0300

    lab01

--- Lab1/Purple.cs
+++ Lab1/Purple.cs
@@ -4,85 +4,84 @@
     {
         public bool Task1(int a, int b, int c)
         {
-            bool answer = false;
-
-            // code here
-
-            // end
-
-            return answer;
+            bool allPositive = (a > 0) && (b > 0) && (c > 0);
+            bool allNegative = (a < 0) && (b < 0) && (c < 0);
+            return allPositive ||  allNegative;
         }
         public bool Task2(int a, int b)
         {
-            bool answer = false;
-
-            // code here
-
-            // end
-
-            return answer;
+            return a == 0 || b == 0 || (a % b == 0) || (b % a == 0);
         }
         public bool Task3(int a, int b)
         {
-            bool answer = false;
-
-            // code here
-
-            // end
-
-            return answer;
+            return a * a == b || a * a * a == b || b * b == a || b * b * b == a;
         }
         public double Task4(double d, double f, double g)
         {
-            double answer = 0;
-
-            // code here
-
-            // end
-
-            return answer;
+            // dx^2 + fx + g = 0
+            return f * f - 4 * d * g;
         }
         public double Task5(double x)
         {
-            double answer = 0;
+            if (x <= -1)
+            {
+                return 1;
+            }
 
-            // code here
+            if (x < 1)
+            {
+                return -x;
+            }
 
-            // end
-
-            return answer;
+            return -1;
         }
         public bool Task6(double squareS, double circleS)
         {
-            bool answer = false;
-
-            // code here
-
-            // end
-
-            return answer;
+            if (squareS <= 0 || circleS <= 0)
+            {
+                return false;
+            }
+            // radius >= a*sqrt(2)/2
+            // S = pi * r^2 => S / pi >= a^2 / 2 => S >= pi * a^2 / 2; a^2 = squareS
+            const double eps = 1e-12;
+            return circleS + eps >= Math.PI * squareS / 2;
         }
 
         public int Task7(bool s, bool t, bool f)
         {
-            int answer = 0;
+            if (s && t)
+            {
+                return 6;
+            }
 
-            // code here
+            if (s && !t)
+            {
+                return f ? 10 : 2;
+            }
 
-            // end
+            if (!t && !f)
+            {
+                return 1;
+            }
 
-            return answer;
+            return t ? 3 : 5;
         }
         public bool Task8(int year, int pupils, int salary)
         {
-            bool answer = false;
             const int bank = 10000;
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+            {
+                return false;
+            }
 
-            // code here
-            
-            // end
+            if (pupils <= 0)
+            {
+                return true;
+            }
 
-            return answer;
+            int aurorsNum = (pupils + 6) / 7;
+            long totalCost = (long)aurorsNum * salary + (long)pupils * 5;
+            return totalCost <= bank;
         }
     }
 }
