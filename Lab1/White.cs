c5b45311ea8ec9adf80385359a25f1df3e2bd73c
ritxx238 <zarita4815@gmail.com>
2025-09-25 16:26:48 +0300

    Update White.cs

--- Lab1/White.cs
+++ Lab1/White.cs
@@ -1,4 +1,4 @@
-﻿namespace Lab1
+namespace Lab1
 {
     public class White
     {
@@ -7,17 +7,33 @@
             bool answer = false;
 
             // code here
+            if (d > 0)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+            Console.WriteLine(answer);
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task2(int n)
         {
             bool answer = false;
 
             // code here
-
+            if (n % 2 == 0)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+            Console.WriteLine(answer);
             // end
 
             return answer;
@@ -27,7 +43,14 @@
             int answer = 0;
 
             // code here
-
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;   
+            }
             // end
 
             return answer;
@@ -37,6 +60,15 @@
             double answer = 0;
 
             // code here
+            if (Math.Abs(d) > Math.Abs(f))
+            {
+                return(f);
+            }
+            else
+            {
+                return (d);
+            }
+
 
             // end
 
@@ -47,7 +79,14 @@
             double answer = 0;
 
             // code here
-
+            if (Math.Abs(x) > 1)
+            {
+                return (1);
+            }
+            else
+            {
+                return(x);
+            }
             // end
 
             return answer;
@@ -57,10 +96,18 @@
             bool answer = false;
 
             // code here
+            if (Math.Abs(Math.Pow(x, 2) + Math.Pow(y, 2) - Math.Pow(r, 2)) <= Math.Pow(10, -4))
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+            Console.WriteLine(answer);
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
 
         public bool Task7(int n)
@@ -68,17 +115,37 @@
             bool answer = false;
 
             // code here
+            int s = n * n;
+            if ((s - n) > (2 * n))
+            {
+                if (n % 2 == 0)
+                {
+                    answer = true;
+                }
+                else
+                {
+                    return answer;
+                }
+            }
+            else
+            {
+                answer = false;
+            }
+            Console.WriteLine(answer);
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task8(double L, int T, int M)
         {
             bool answer = false;
 
             // code here
-
+            if (L <= 30 && T + M >= 5 && M % 2 == 0)
+            {
+                answer = true;
+            }
+            Console.WriteLine(answer);
             // end
 
             return answer;
