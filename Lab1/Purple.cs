f266bf62dbc5cb9502eb990f0a1b501d9af5b04c
Bondar M.P <m2500458@edu.misis.ru>
2025-10-01 01:12:18 +0300

    Add files via upload

--- Lab1/Purple.cs
+++ Lab1/Purple.cs
@@ -7,7 +7,10 @@
             bool answer = false;
 
             // code here
-
+            if (((a > 0) && (b > 0) && (c > 0)) || ((a < 0) && (b < 0) && (c < 0)))
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -17,7 +20,10 @@
             bool answer = false;
 
             // code here
-
+            if (((b != 0) && (a % b == 0)) || ((a != 0) && (b % a == 0)))
+            {
+                answer |= true;
+            }
             // end
 
             return answer;
@@ -27,7 +33,10 @@
             bool answer = false;
 
             // code here
-
+            if ((a * a == b) || (a * a * a == b) || (b * b == a) || (b * b * b == a))
+            {
+                answer |= true;
+            }
             // end
 
             return answer;
@@ -37,7 +46,7 @@
             double answer = 0;
 
             // code here
-
+            answer = f * f - 4 * d * g;
             // end
 
             return answer;
@@ -47,7 +56,18 @@
             double answer = 0;
 
             // code here
-
+            if (x <= -1)
+            {
+                answer = 1;
+            }
+            else if ((x >= -1) && (x <= 1))
+            {
+                answer = -x;
+            }
+            else
+            {
+                answer = -1;
+            }
             // end
 
             return answer;
@@ -57,7 +77,10 @@
             bool answer = false;
 
             // code here
-
+            if (circleS / Math.PI * 4 >= squareS * 2)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -68,7 +91,42 @@
             int answer = 0;
 
             // code here
-
+            if (s == true)
+            {
+                if (t == true)
+                {
+                    answer = 6;
+                }
+                else
+                {
+                    if (f == true)
+                    {
+                        answer = 10;
+                    }
+                    else
+                    {
+                        answer = 2;
+                    }
+                }
+            }
+            else
+            {
+                if (t == true)
+                {
+                    answer = 3;
+                }
+                else
+                {
+                    if (f == true)
+                    {
+                        answer = 5;
+                    }
+                    else
+                    {
+                        answer = 1;
+                    }
+                }
+            }
             // end
 
             return answer;
@@ -79,7 +137,23 @@
             const int bank = 10000;
 
             // code here
-            
+            if (!(year % 4 == 0 && year % 100 != 0 || year % 400 == 0))
+            {
+                if (pupils % 7 != 0)
+                {
+                    if ((pupils / 7 + 1) * salary + 5 * pupils <= bank)
+                    {
+                        answer = true;
+                    }
+                }
+                else
+                {
+                    if (pupils / 7 * salary + 5 * pupils <= bank)
+                    {
+                        answer = true;
+                    }
+                }
+            }
             // end
 
             return answer;
